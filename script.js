8d3b43fff3dc1a02a73e45da79b61c963d029880
gabrieleinsfeld <gabrieleinsfeld@gmail.com>
2024-07-09 15:16:58 -0300

    Added logic to check gameOver

--- script.js
+++ script.js
@@ -15,6 +15,9 @@ function createBoard(){
                 cell.textContent = curPlayer
                 curPlayer = setPlayer(curPlayer)
             }
+            if(isGameOver(board)){
+                console.log("Game Over")
+            }
         })
         cell.textContent = element
         container.appendChild(cell)
@@ -44,8 +47,38 @@ function makeMove(player){
 
 }
 
-function isGameOver(){
-
+function isGameOver(board){
+    for (let index = 0; index < 9; index+=3) {
+        if(board[index.toString()] == board[(index+1).toString()] && board[index.toString()] == board[(index+2).toString()]){
+            if (board[index.toString()] != ""){
+                return true
+            }else{
+                return false
+            }
+        }
+        
+    }
+    for (let index = 0; index < 3; index+=1) {
+        if(board[index.toString()] == board[(index+3).toString()] && board[index.toString()] == board[(index+6).toString()]){
+            if (board[index.toString()] != ""){
+                return true
+            }else{
+                return false
+            }
+        }
+        
+    }
+    if(board["0"] == board["4"] && board["0"] == board["8"]){
+        if (board["0"] != ""){
+            return true
+        }
+    }else if(board["2"] == board["4"] && board["2"] == board["6"]){
+        if (board["2"] != ""){
+            return true
+        }
+    }else{
+        return false
+    }
 }
 
 function isMoveLegal(board,index){
