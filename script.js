84c3f7187ae01c4a8fc9357a3fb37f54cb7a67d0
gabrieleinsfeld <gabrieleinsfeld@gmail.com>
2024-07-09 16:10:03 -0300

    Added coments

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ curPlayer = ""
 
 function createBoard(board){
     const container = document.getElementById("container")
+
+    // Erases current board if it exists
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
     
+    // Add cells and event listeners for each  cell
     for (let index in board) {
         const element = board[index];
         const cell = document.createElement("div")
         cell.setAttribute("id", `cell${index}`)
         cell.setAttribute('class', 'cell')
         cell.addEventListener("click", ()=>{
+            // Click checks if move is legal and if game is Over
             if(isMoveLegal(board, index)){
                 board[index] = curPlayer
                 cell.textContent = curPlayer
@@ -28,6 +32,9 @@ function createBoard(board){
     return board
 }
 
+
+
+// Sets the player, at first is random
 function setPlayer(currentPlayer=""){
     let player = ""
     if(currentPlayer == ""){
@@ -46,6 +53,7 @@ function setPlayer(currentPlayer=""){
 }
 
 
+// Sets game over screen and add event listener to restart button
 function gameOver(){
     const gameOverDisplay = document.getElementById("game_over")
     gameOverDisplay.style.display = "flex"
@@ -57,7 +65,11 @@ function gameOver(){
 }
 
 
+
+// Loops through cells to see if game is over
 function isGameOver(board){
+
+    // Checks if rows are equal
     for (let index = 0; index < 9; index+=3) {
         if(board[index.toString()] == board[(index+1).toString()] && board[index.toString()] == board[(index+2).toString()]){
             if (board[index.toString()] != ""){
@@ -66,6 +78,8 @@ function isGameOver(board){
         }
         
     }
+
+    // Checks if columns are equal
     for (let index = 0; index < 3; index+=1) {
         if(board[index.toString()] == board[(index+3).toString()] && board[index.toString()] == board[(index+6).toString()]){
             if (board[index.toString()] != ""){
@@ -74,6 +88,8 @@ function isGameOver(board){
         }
         
     }
+
+    // Checks if diagonals are equal
     if(board["0"] == board["4"] && board["0"] == board["8"]){
         if (board["0"] != ""){
             return true
@@ -84,6 +100,7 @@ function isGameOver(board){
         }
     }
 
+    // Cheks if all elements are different than ""
     if(Object.values(board).every(value => value !== "")){
         return true
     }
